5ca87168eb3bd21149e3540a64e8c31b40df1011
unknown <camilo.quintero-r@labinfo.is.escuelaing.edu.co>
2025-09-09 08:58:08 -0500

    primer commit

---
+++ src/main/java/com/mycompany/parcial1/arep/Parcial1Arep.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.parcial1.arep;
+
+/**
+ *
+ * @author CAMILO.QUINTERO-R
+ */
+public class Parcial1Arep {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
